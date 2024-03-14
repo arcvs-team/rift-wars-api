724e684640ca052a93f0ed7228209ff63d5a3781
Matheus Sartori <ms.sartori@outlook.com.br>
2024-03-14 12:21:35 -0300

    feat: add team player entity and factory

---
+++ src/domain/team/enterprise/entities/team-player.ts
@@ -0,0 +1,48 @@
+import { Entity } from '@/core/entities/entity'
+import { type UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { type Optional } from '@/core/types/optional'
+
+export interface TeamPlayerAttributes {
+  playerId: UniqueEntityID
+  teamId: UniqueEntityID
+  joinedAt?: Date
+  removedAt?: Date
+  removedBy?: UniqueEntityID
+}
+
+export class TeamPlayer extends Entity<TeamPlayerAttributes> {
+  get playerId () {
+    return this.attributes.playerId
+  }
+
+  get teamId () {
+    return this.attributes.teamId
+  }
+
+  get joinedAt () {
+    return this.attributes.joinedAt
+  }
+
+  get removedAt () {
+    return this.attributes.removedAt
+  }
+
+  get removedBy () {
+    return this.attributes.removedBy
+  }
+
+  static create (
+    attributes: Optional<TeamPlayerAttributes, 'joinedAt' | 'removedAt' | 'removedBy'>,
+    id?: UniqueEntityID
+  ) {
+    const teamPlayer = new TeamPlayer(
+      {
+        ...attributes,
+        joinedAt: attributes.joinedAt ?? new Date()
+      },
+      id
+    )
+
+    return teamPlayer
+  }
+}
---
+++ test/factories/make-team-player.ts
@@ -0,0 +1,20 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { TeamPlayer, type TeamPlayerAttributes } from '@/domain/team/enterprise/entities/team-player'
+import { faker } from '@faker-js/faker'
+
+export function makeTeamPlayer (
+  override: Partial<TeamPlayerAttributes> = {},
+  id?: UniqueEntityID
+) {
+  const teamPlayer = TeamPlayer.create(
+    {
+      playerId: new UniqueEntityID(faker.string.uuid()),
+      teamId: new UniqueEntityID(faker.string.uuid()),
+      joinedAt: faker.date.recent(),
+      ...override
+    },
+    id
+  )
+
+  return teamPlayer
+}
