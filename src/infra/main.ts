b2d63cb6b95df40a750352f4de69a82f56061a6b
Matheus Sartori <ms.sartori@outlook.com.br>
2024-04-10 14:00:19 -0300

    refactor: separate responsibilities

--- src/infra/main.ts
+++ src/infra/main.ts
@@ -1,75 +1,18 @@
 import 'reflect-metadata'
-import 'dotenv/config'
-import { ApolloServer } from '@apollo/server'
-import { expressMiddleware } from '@apollo/server/express4'
-import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer'
-import express from 'express'
-import http from 'http'
-import cors from 'cors'
-import { buildSchema } from 'type-graphql'
-import { PlayerResolver } from './graphql/apollo-server/resolvers/player.resolver'
-import { TeamResolver } from './graphql/apollo-server/resolvers/team.resolver'
-import { TournamentResolver } from './graphql/apollo-server/resolvers/tournament.resolver'
-import path from 'path'
-import { verify } from 'jsonwebtoken'
-import { container } from '@/infra/container/inversify'
-import { type FetchPlayerByIdUseCase } from '@/domain/player/application/use-cases/fetch-player-by-id'
+import { ApolloGraphQLServer } from './graphql/apollo-server/apollo-graphql-server'
+import { ExpressHttpServer } from './http/express/express-http-server'
 
-async function server () {
-  const app = express()
-  const httpServer = http.createServer(app)
+async function bootstrap () {
+  const expressHttpServer = new ExpressHttpServer()
 
-  const schema = await buildSchema({
-    resolvers: [
-      PlayerResolver,
-      TeamResolver,
-      TournamentResolver
-    ],
-    emitSchemaFile: path.resolve(__dirname, 'schema.gql')
-  })
+  const apolloGraphQLServer = new ApolloGraphQLServer(expressHttpServer.getHttpServer())
+  await apolloGraphQLServer.buildSchema()
+  await apolloGraphQLServer.setupServer()
 
-  // Set up Apollo Server
-  const server = new ApolloServer({
-    schema,
-    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
-  })
-  await server.start()
+  const apolloServer = apolloGraphQLServer.getServer()
 
-  app.use(
-    cors(),
-    express.json()
-  )
-
-  app.use('/graphql', expressMiddleware(server, {
-    context: async ({ req, res }) => {
-      const accessToken = req.headers.authorization ?? ''
-
-      if (accessToken === null || accessToken === undefined || accessToken.length === 0) return {}
-
-      const [, token] = accessToken.split(' ')
-
-      const decoded = verify(token, String(process.env.JWT_SECRET))
-
-      const { sub } = decoded
-
-      const fetchPlayerByIdUseCase: FetchPlayerByIdUseCase = container.get('FetchPlayerByIdUseCase')
-
-      try {
-        const result = await fetchPlayerByIdUseCase.execute({ userId: String(sub) })
-
-        if (result.isRight()) {
-          return { player: result.value.player }
-        }
-
-        return {}
-      } catch {
-        return {}
-      }
-    }
-  }))
-
-  httpServer.listen({ port: 4000 })
-  console.log('🚀 Server ready at http://localhost:4000')
+  expressHttpServer.configureMiddleware(apolloServer)
+  expressHttpServer.start()
 }
 
-server()
+bootstrap()
